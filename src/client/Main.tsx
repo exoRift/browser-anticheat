f390b685bc1430b4f27ad8b9a9ada1d501968d2f
Arthur Neuman <arthurbneuman@gmail.com>
2025-01-26 17:14:23 -0500

    Basic setup

---
+++ src/client/Main.tsx
@@ -0,0 +1,3 @@
+export default function Main () {
+  return <div className='text-2xl text-red-500'>bruh</div>
+}
---
+++ src/server/index.ts
@@ -0,0 +1,41 @@
+import polka from 'polka'
+import { json } from 'body-parser'
+import serve from 'sirv'
+
+import { sendType } from './middleware/sendtype'
+
+const {
+  PORT,
+  NODE_ENV
+} = process.env
+
+// Define server
+const app = polka()
+app
+  .use(json())
+  .use(sendType)
+
+// Attach frontend
+if (NODE_ENV === 'production') {
+  app.use(serve('build', {
+    single: true,
+    ignores: '/api/*'
+  }))
+} else {
+  const { createServer: createViteServer } = await import('vite')
+
+  const vite = await createViteServer({
+    server: { middlewareMode: true }
+  })
+
+  app.use(vite.middlewares)
+}
+
+// Define controllers
+app
+  .all('/api', (req, res) => void res.status(200).end())
+
+// Listen
+app.listen(PORT, () => {
+  console.info('Server online listening at http://localhost:%s', PORT)
+})
