93f9189caa50ff9846825b871d0a3cebe69ac3ed
Arthur Neuman <arthurbneuman@gmail.com>
2025-01-26 19:35:37 -0500

    Re-setup linter

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import neostandard from 'neostandard'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  ...neostandard({
+    ignores: ['build/'],
+    ts: true,
+  }),
+  {
+    name: 'overrides',
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/no-non-null-assertion': 'warn'
+    }
+  }
+]
